0dd5e2822f81e9538b56cfc314a688a50b1bf16e
EvgenSharapov <dooplert@yandex.ru>
2025-03-04 23:24:05 +0300

    Initial commit

---
+++ src/test/java/org/example/lib/TestLibApplication.java
@@ -0,0 +1,11 @@
+package org.example.lib;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestLibApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.from(LibApplication::main).with(TestcontainersConfiguration.class).run(args);
+    }
+
+}
